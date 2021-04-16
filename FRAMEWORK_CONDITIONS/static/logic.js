b03095e90da8f9945135c742a7305f22b778d513
pratixashah <shah.pratixa@gmail.com>
2021-04-16 10:26:16 -0700

    Create logic.js

---
+++ FRAMEWORK_CONDITIONS/static/logic.js
@@ -0,0 +1,9 @@
+
+
+d3.json("http://localhost:5000/api").then(function(data) {
+    // Once we get a response, send the data.features object to the createFeatures function
+    console.log(data);
+
+    
+      
+  });
